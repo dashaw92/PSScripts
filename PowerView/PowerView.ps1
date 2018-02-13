46033570d52cd4358c73cb7aba76254286b40604
dashaw92 <dashaw92@gmail.com>
2018-02-12 22:04:10 -0500

    Initial, add PowerView

---
+++ PowerView/PowerView.ps1
@@ -0,0 +1,78 @@
+﻿<#
+PowerView - An OSRS player stat viewer
+February 12, 2018
+By Daniel Shaw
+
+This script can either be run as a standalone to view stats, or in an interactive environment:
+
+#Export selected rows in Out-GridView to a CSV file
+PS> .\PowerView.ps1 | Export-Csv -Path .\stats.csv
+
+#Output selected rows to JSON
+PS> .\PowerView.ps1 | ConvertTo-Json
+#>
+
+[void][Reflection.Assembly]::LoadWithPartialName('Microsoft.VisualBasic')
+
+Function To-Name($Index) {
+    switch($Index) {
+        0  { "Total"        }
+        1  { "Attack"       }
+        2  { "Defence"      }
+        3  { "Strength"     }
+        4  { "Hitpoints"    }
+        5  { "Ranged"       }
+        6  { "Prayer"       }
+        7  { "Magic"        }
+        8  { "Cooking"      }
+        9  { "Woodcutting"  }
+        10 { "Fletching"    }
+        11 { "Fishing"      }
+        12 { "Firemaking"   }
+        13 { "Crafting"     }
+        14 { "Smithing"     }
+        15 { "Mining"       }
+        16 { "Herblore"     }
+        17 { "Agility"      }
+        18 { "Thieving"     }
+        19 { "Slayer"       }
+        20 { "Farming"      }
+        21 { "Runecraft"    }
+        22 { "Hunter"       }
+        23 { "Construction" }
+    }
+}
+
+Function To-Stat($Line, $Index) {
+    if($Line.Split(",").Length -ige 3) {
+        $content = $Line.Split(",");
+        return [PSCustomObject]@{
+            Skill = To-Name($Index)
+            Rank = [int]("{0:N0}" -f [int]$content[0])
+            Level = [int]$content[1]
+            Experience = [int]("{0:N0}" -f [int]$content[2])
+        }
+    }
+}
+
+$url = "services.runescape.com/m=hiscore_oldschool/index_lite.ws?player="
+$name = [Microsoft.VisualBasic.Interaction]::InputBox("Enter your OSRS display name", "PowerViewer")
+try {
+    $request = Invoke-WebRequest -Uri ("{0}{1}" -f $url, $name)
+} catch {
+    Write-Error "`"$name`" is not a valid OSRS display name." -Category InvalidArgument -ErrorId "1"
+    return
+} 
+
+$raw_data = $request.Content.Split("`n")
+#Will contain the completely parsed stats.
+$data = @()
+$index = 0
+foreach($i in $raw_data) {
+    #This excludes all non-skill based lines because honestly, who cares.
+    if(-Not [string]::IsNullOrEmpty($i.Trim())) {
+        $data += (To-Stat -Line $i -Index $index);
+        $index++
+    }
+}
+$data | Select Skill,Rank,Level,Experience | Out-GridView -Title ("{0}{1}" -f "PowerView | ", $name) -PassThru
---
+++ PowerView/readme.md
@@ -0,0 +1,15 @@
+# PowerView  
+An OldSchool RuneScape player stats viewer.  
+This script can be run both normally and interactively.  
+When run interactively, output can be manipulated via any compatible cmdlet.  
+Example: Exporting data into a CSV file:  
+```powershell
+PS> .\PowerView.ps1 | Export-Csv -Path .\stats.csv
+```
+
+### Disclaimer  
+I just started learning PowerShell the day before the time of writing (February 12, 2018), so I apologize for any poor quality code.  
+OldSchool RuneScape is owned by Jagex Ltd.
+
+### License  
+MIT
